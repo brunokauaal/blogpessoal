20275ef7ac693282f423b5bad561f3615cfab17f
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-04-13 12:08:38 -0300

    Meu Blog pessoal

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,12 @@
 package com.generation.blogpessoal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
                  //Nome da interface                    //nome da class dps a chave 
+
+@Repository  //eliminar assim que o guilherme arrumar a validaçao
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
 }
