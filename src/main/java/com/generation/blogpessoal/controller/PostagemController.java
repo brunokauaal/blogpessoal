eafbf4be369ee47ce9da10aceace062b6e57c217
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-04-17 17:33:09 -0300

    Parte do tema e postagem com um exercicio que irei finalizar assim que chegar em casa

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,9 +20,9 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 
 @RestController // indicar que classe e controladora
 @RequestMapping("/postagens") // endereço
@@ -30,9 +30,13 @@ import jakarta.validation.constraints.NotNull;
 public class PostagemController {
 
 	@Autowired // Injeçao de dependencia apartir de agr tu Spring cria objetos manipula dados
-				// dleta e etc para ter acesso ao dados do db
+	// dleta e etc para ter acesso ao dados do db
 	private PostagemRepository postagemRepository;
 
+	@Autowired
+	private TemaRepository TemaRepository;
+	
+	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll() { // resposta http status no caso vai trazer lista de postagens que
 														// tem la dentro fizemos uma listagem da nossa tabela
@@ -96,8 +100,12 @@ public class PostagemController {
 	
 			if(postagem.isEmpty())
 	            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-
-	        postagemRepository.deleteById(id);
+			else
+				postagemRepository.deleteById(id);
+			
+		throw new ResponseStatusException(HttpStatus.OK);
+		
+		//Não sei se deixo status ok ou nocontent 
 		
 		/* DELETE * FROM tb_postagens WHERE  id= id*/
 	}
